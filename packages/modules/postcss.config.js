781c74ab900a0036a7ad275dc8ac9b069545039f
dongxingpeng <291785613@qq.com>
2019-09-27 13:16:28 +0800

    vue-cli3脚手架

---
+++ packages/modules/postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: {
+    autoprefixer: {},
+    "postcss-pxtorem-multi": {
+      rootValue: 37.5,
+      propList: ['*'],
+    }
+  }
+};
---
+++ packages/modules/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p class="title"> This is Home Page！！！ </p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
+<style lang="less">
+  .title{
+  }
+</style>
