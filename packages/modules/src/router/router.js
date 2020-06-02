52fc03f133cb447103936e2c688613b906ead024
dongxingpeng <291785613@qq.com>
2020-06-02 08:56:40 +0800

    删除文件，练习lerna

--- packages/modules/src/router/router.js
+++
@@ -1,9 +0,0 @@
-const  Home =  ()=> import(/* webpackChunkName: "home" */ '../views/Home.vue');
-const urlRouter = [
-    {
-        path: '/',
-        name: 'Home',
-        component: Home
-    },
-];
-export default urlRouter;
